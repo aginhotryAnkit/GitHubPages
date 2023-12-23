4520b32ba6750776e1bfe06904585988b41fe207
ankit aginhotry <akaginhotry9651@gmail.com>
2023-12-23 20:02:47 +0530

    remove functinaly workin gon

--- todo/script.js
+++ todo/script.js
@@ -1,51 +1,82 @@
 var todoList = {};
-var count=0;
+var count = 0;
+
+$(document).ready(function () {
+    //update the element from the list
+    $(".update").click(function (e) {
+        var childID = $(this).attr("id");
+
+        // $("#li"+childID).remove();
+    });
+
+    //delete the element from the list
+    $(".delete").click(function (e) {
+        var childID = $(this).attr("id");
+        console.log("hello");
+        // $("#li"+childID).remove();
+        // var todoList = deleteTask(childID, todoList);
+        // displayTodoList();
+    });
+
+    //function to create a todo
+    $("#todoCreateBtn").click(function (e) {
+        e.preventDefault();
+        let todoText = $("#newTodo").val();
+        var duplicateCheck = ((value, todoList) => {
+            return Object.values(todoList).includes(value);
+        })(todoText, todoList);
+
+        if (duplicateCheck) {
+            alert("Task is already exist");
+        } else {
+            todoList[count + "ABC"] = todoText;
+            count++;
+        }
+
+        displayTodoList();
+    });
+});
+
+//remove the task from the object
+function deleteTask(key, obj = {}) {
+    console.log("before delete");
+    console.log(obj);
+    var newObj = {};
+    var keys = Object.keys(obj);
+
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] !== key) {
+            newObj[keys[i]] = obj[keys[i]];
+        }
+        console.log(obj.hasOwnProperty(keys[i]));
+    }
+    console.log("before delete");
+
+    console.log(newObj);
+    return newObj;
+}
 
 //display todo list
-function displayTodoList(){
-    console.log("//");
+function displayTodoList() {
     let htmStr = "";
-    Object.keys(todoList).forEach(element=>{
-        console.log(todoList[element]);
-        htmStr += `<li id="${element}">
+    Object.keys(todoList).forEach((element) => {
+        htmStr += `<li id="${"li" + element}">
         <div class="content">
             <h3>Default Title</h3>
             <p>${todoList[element]}</p>
         </div>
         <div class="buttons">
-            <button class="delete" id="${element+"Delete"}  onclick="deleteItem(${count})">Delete</button>
-            <button class="update" id="${element+"Update"}  onclick="updateItem(${count})">Update</button>
+            <button class="delete" id="${element} ">Delete</button>
+            <button class="update" id="${element + "Update"} ">Update</button>
         </div> 
         </li>`;
         count++;
     });
-
     $("ul").html(htmStr);
+    console.log("Display RUn");
+
+    $(".delete").click(function (e) { 
+        var childID = $(this).attr("id");
+         $("#li"+childID).remove();
+    });
 }
-
-
-$(document).ready(function () {
-    console.log("Jquery Working Fine");
-
-    //function to create a todo 
-    $("#todoCreateBtn").click(function (e) { 
-        e.preventDefault();
-        let todoText = $("#newTodo").val();
-        var duplicateCheck = ((value,todoList)=>{
-            return Object.values(todoList).includes(value);
-        })(todoText, todoList);
-
-        if(duplicateCheck){
-            alert("Task is already exist");
-        } else {
-            todoList[count+"ABC"]=todoText;
-            count++;
-        }
-        displayTodoList();
-    });
-   
-    //not clickable
-    $(".update").click(function(e){
-        console.log(e);
-    });
-});
