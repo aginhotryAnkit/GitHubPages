f5c7e13be85342f47ba74c6a11320518175183de
ankit aginhotry <akaginhotry9651@gmail.com>
2024-01-27 21:36:18 +0530

    password validation and also apply some css to check the validation point like uppercase smallcase and spacial characters

---
+++ C Program/area copy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,area;
+    printf("inter the value of length  :");
+scanf("%d",a);
+printf("inter the value of width :");
+scanf("%d",b);
+area=a*b;
+printf("area of rectangle", area);
+getch();
+}
---
+++ C Program/divisible.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("\n ENTER A NUMBER");
+scanf("%d",&num);
+if("num % 2==0")
+{
+    printf("\n ENTERED THE NUMBER IS DIVISIBLE BY 2");
+
+}
+if("num % 3==0");
+{
+    printf("\n ENTERED THE NUMBER IS DIVISIBLE BY 3");
+}
+return 0;
+}
