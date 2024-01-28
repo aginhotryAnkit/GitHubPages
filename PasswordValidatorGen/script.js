24cbe11aa2d85ecc04e1ce781d1226e4ff62d071
ankit aginhotry <akaginhotry9651@gmail.com>
2024-01-28 22:26:57 +0530

    code optimized for password validator

--- PasswordValidatorGen/script.js
+++ PasswordValidatorGen/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    console.log("jQuery working");
+    console.log("Developed By Ankit Aginhotry\nGithub: aginhotryAnkit");
 
     // global vars 
     const specialCharacters = [
         '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@',
-        '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ' ', // including space as a special character
+        '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',
     ];
 
-
+    //flag for hide and unhide the password field
     var password = true;
     $(".progress").css("width", "30%");
 
@@ -30,6 +30,7 @@ $(document).ready(function () {
         $(".pass").attr("type", "text");
     });
 
+    //function for managing the password complexity progressBar and info text
     function validator(currentVal){
         currentVal = currentVal.replace(/\s/g, '');
         let duplicate = [];
@@ -37,7 +38,7 @@ $(document).ready(function () {
         let upperCaseCharCount = 0;
         let lowerCaseCharCount = 0;
         let numericCount = 0;
-        let result = 0;
+        let passwordComplexity = 0;
 
         //count the special char from the given text
         currentVal.split("").map((ele,acc)=>{
@@ -53,26 +54,25 @@ $(document).ready(function () {
             duplicate.push(ele);
         }, 0);
 
-        result += countSpacialCharCount*19+upperCaseCharCount*5+lowerCaseCharCount*5+numericCount*5;
-        console.log(result);
-        if(result>100){
-            console.log(result);
-            result = 100;
-           
+        //calculating the password complexity
+        passwordComplexity += countSpacialCharCount*19+upperCaseCharCount*5+lowerCaseCharCount*5+numericCount*5;
+        console.log(passwordComplexity);
+        if(passwordComplexity>100){
+            passwordComplexity = 100;
         }
 
-
-        if(result>=0 && result<=30){
+        //Changing the progressBar on the basic of complexity of password
+        if(passwordComplexity>=0 && passwordComplexity<=30){
             //poor
             $(".progress").css("width", "30%");
             $(".progress").css("background", "red");
             
-        } else if (result>30 && result<=55){
+        } else if (passwordComplexity>30 && passwordComplexity<=55){
             //avg
             $(".progress").css("width", "60%");
             $(".progress").css("background", "#FFC000");
 
-        } else if(result>55 && result<=100){
+        } else if(passwordComplexity>55 && passwordComplexity<=100){
             //excellent
             if(countSpacialCharCount>0){
                 $(".progress").css("width", "100%");
@@ -83,35 +83,18 @@ $(document).ready(function () {
             }
         }
 
+        // by default the the li is being red
         $("li").css("color", "red");
 
-        if(currentVal.length>=8){
-            $("#eightChar").css("color", "green");
-        }
+        //changing the info text as the basic of password length, case and chars. 
+        currentVal.length>=8?$("#eightChar").css("color", "green"):"";
+        lowerCaseCharCount>0? $("#oneLowerCase").css("color", "green"):"";
+        upperCaseCharCount>0? $("#oneUpperCase").css("color", "green"):"";
+        countSpacialCharCount>0? $("#oneSpacialChar").css("color", "green"):"";
+        currentVal.length==0?$(".progress").css("width", "0%"):"";
 
-        if(lowerCaseCharCount>0){
-            //green
-            $("#oneLowerCase").css("color", "green");
-
-        }
-
-        if(upperCaseCharCount>0){
-            //green
-            $("#oneUpperCase").css("color", "green");
-
-        }
-
-        if(countSpacialCharCount>0){
-            //green
-            $("#oneSpacialChar").css("color", "green");
-
-        }
-
-        if(currentVal.length==0){
-            $(".progress").css("width", "0%");
-        }
-
-        // console.log("total result =", result);
+        //console debugging code
+        // console.log("total passwordComplexity =", passwordComplexity);
         // console.log(currentVal[currentVal.length-1]);
         // console.log("Spacial Character :", countSpacialCharCount);
         // console.log("Uppercase :", upperCaseCharCount);
@@ -119,7 +102,7 @@ $(document).ready(function () {
         // console.log("Numeric :", numericCount);
     }
 
-
+    //function for creating a strong password
     function generateRandomStrongPassword(length) {
         // Define characters to use in the password
         const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
@@ -139,6 +122,7 @@ $(document).ready(function () {
         return password;
     }
 
+    //event append the generate song password to the password field
     $(".genBtn").click(function (e) { 
         e.preventDefault();
         let password = generateRandomStrongPassword(Math.floor(Math.random() * 3)+8)
@@ -146,6 +130,7 @@ $(document).ready(function () {
         validator(password);
     });
 
+    //when the key is pressed also check the password complexity and update the ui accordingly
     $(".pass").keydown(function (e) { 
         let currentVal = $(this).val();
         console.log("hello");
